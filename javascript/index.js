0815564e0e72034403f7b5afcee1ec4222459c3b
DCClara <141446800+DCClara@users.noreply.github.com>
2023-09-24 15:45:11 -0400

    added another city, fixed seconds interval bug,  slight change to css

--- javascript/index.js
+++ javascript/index.js
@@ -4,11 +4,13 @@ function updateTime() {
     let newYorkDateElement = newYorkElement.querySelector(".date");
     let newYorkTimeElement = newYorkElement.querySelector(".time");
     let newYorkTime = moment().tz("America/New_York");
+
     newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
     newYorkTimeElement.innerHTML = newYorkTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+
   let romeElement = document.querySelector("#rome");
   if (romeElement) {
     let romeDateElement = romeElement.querySelector(".date");
@@ -17,13 +19,32 @@ function updateTime() {
     romeDateElement.innerHTML = romeTime.format("MMMM Do YYYY");
     romeTimeElement.innerHTML = romeTime.format("h:mm:ss [<small>]A[</small>]");
   }
+
+  let tokyoElement = document.querySelector("#tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
+let cityInterval = null;
+
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  clearInterval(cityInterval);
+
+  setCityInterval(cityTimeZone, cityName);
+  cityInterval = setInterval(setCityInterval, 1000, cityTimeZone, cityName);
+}
+function setCityInterval(cityTimeZone, cityName) {
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
@@ -39,8 +60,8 @@ function updateCity(event) {
         <a href="/">Back to home</a>`;
 }
 
-updateTime();
-setInterval(updateTime, 1);
-
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
+
+updateTime();
+setInterval(updateTime, 1000);
