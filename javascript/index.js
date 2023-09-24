5be518c583ff547d81b21710325490fca689587a
DCClara <141446800+DCClara@users.noreply.github.com>
2023-09-24 12:00:00 -0400

    added moment.js links and created function for current date and time of cities

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
+function updateTime() {
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss:SS [<small>]A[</small>]"
+  );
 
+  let romeElement = document.querySelector("#rome");
+  let romeDateElement = romeElement.querySelector(".date");
+  let romeTimeElement = romeElement.querySelector(".time");
+  let romeTime = moment().tz("Europe/Rome");
+  romeDateElement.innerHTML = romeTime.format("MMMM Do YYYY");
+  romeTimeElement.innerHTML = romeTime.format(
+    "h:mm:ss:SS [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1);
